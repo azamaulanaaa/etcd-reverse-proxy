36a2685705c777e5b902645737b020fe2f323ce2
Aza Maulana <azamaulanaaa@gmail.com>
2025-10-21 13:51:49 +0700

    feat: add verbose or debug mode

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
+use clap::Parser;
 use simple_logger::SimpleLogger;
 
+#[derive(clap::Parser, Debug)]
+#[command(version)]
+struct Args {
+    #[arg(long, default_value_t = false, help = "Set logger to debug mode")]
+    verbose: bool,
+}
+
 fn main() -> anyhow::Result<()> {
-    SimpleLogger::new().init()?;
+    let args = Args::try_parse()?;
+
+    let log_level = if args.verbose {
+        log::LevelFilter::Debug
+    } else {
+        log::LevelFilter::Error
+    };
+    SimpleLogger::new().with_level(log_level).init()?;
 
     log::info!("Hello, world!");
 
